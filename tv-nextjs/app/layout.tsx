6ef050b89eb29bd422d63bd7f693b3cc60227b80
Sourav Bhowal <souravbhowal15@gmail.com>
2026-07-27 00:31:47 +0530

    Add mock hardware scripts for raspberry-pi-1 and raspberry-pi-2 connections

--- tv-nextjs/app/layout.tsx
+++ tv-nextjs/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body>{children}</body>
     </html>
